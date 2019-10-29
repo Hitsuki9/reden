1ff5ca11eb2e097eec78e2f077c39e133f2d77a1
wangxin <wangxin@thothinfo.com>
2019-10-29 09:00:31 +0800

    update: add redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import './index.less';
 import 'normalize.css';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import store from './store';
+
+// 请求 Notification 授权
+if (
+  window.Notification
+  && (window.Notification.permission === 'default' || window.Notification.permission === 'denied')
+) {
+  window.Notification.requestPermission();
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ src/store/reducer.ts
@@ -0,0 +1,3 @@
+function reducer () {}
+
+export default reducer;
