503509d9d1afd9b96d927dafd6a1d8ab515f277e
wangxin <wangxin@thothinfo.com>
2019-10-30 10:47:50 +0800

    update: add actionType

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,3 +1,39 @@
-function reducer () {}
+import {
+  Action,
+  ActionTypes
+} from './action';
+
+interface State {
+  user: {
+    id: string,
+    username: string,
+    avatar: string
+  } | null,
+  connect: boolean
+}
+
+const initialState: State = {
+  user: null,
+  connect: false
+};
+
+function reducer (state: State = initialState, action: Action): State {
+  switch (action.type) {
+    case ActionTypes.Connect:
+      return {
+        ...state,
+        connect: true
+      };
+    case ActionTypes.Disconnect:
+      return {
+        ...state,
+        connect: false
+      };
+    case ActionTypes.SetUser:
+      return {
+        ...state
+      };
+  }
+}
 
 export default reducer;
