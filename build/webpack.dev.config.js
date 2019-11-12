3a8b46cdf7a5ea217efe657f3399814684c5b72b
Hitsuki9 <31876031+Hitsuki9@users.noreply.github.com>
2019-11-12 17:35:27 +0800

    Update webpack.dev.config.js

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -61,7 +61,8 @@ module.exports = {
         test: /\.css$/,
         use: [
           'style-loader',
-          'css-loader'
+          'css-loader',
+          'postcss-loader'
         ]
       }, {
         test: /\.(png|jpe?g)(\?.*)?$/,
