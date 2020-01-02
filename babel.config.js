74aa930cd98d7fc2e13f02547c8ce77fc1d6b6ba
Hitsuki9 <ybfqdjb7788@gmail.com>
2020-01-03 00:18:51 +0800

    update: modify Flipper

--- babel.config.js
+++ babel.config.js
@@ -15,6 +15,12 @@ module.exports = function(api) {
 
   const plugins = [
     '@babel/plugin-transform-runtime',
+    [
+      '@babel/plugin-proposal-class-properties',
+      {
+        loose: true
+      }
+    ],
     'react-hot-loader/babel',
     [
       'import',
--- client/pages/Flipper/Flipper.tsx
+++ client/pages/Flipper/Flipper.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { useHistory } from 'react-router-dom';
 import classNames from 'classnames';
 import styles from './Flipper.less';
 
-class Panel {
-  config = {
-    front: 'num0',
-    back: 'num1'
-  };
-  constructor() {}
-  init() {}
-  setFront(className: string) {}
-  setBack(className: string) {}
-  flip() {}
-}
+// class Panel {
+//   config = {
+//     front: 'num0',
+//     back: 'num1'
+//   };
+//   constructor(config: { front: string; back: string }) {
+//     Object.assign(this.config, config);
+//     this.init();
+//   }
+//   private init() {
+//     this.setFront(this.config.front);
+//     this.setBack(this.config.back);
+//   }
+//   setFront(className: string) {}
+//   setBack(className: string) {}
+//   flip() {}
+// }
 
 /**
  * 翻牌时钟
@@ -21,19 +27,36 @@ class Panel {
 export default function Flipper() {
   const history = useHistory();
 
+  const renderPanel = (n = 6) => {
+    const panels: JSX.Element[] = [];
+    for (let i = 0; i < n; i++) {
+      panels.push(
+        <Fragment key={i}>
+          <div className={classNames(styles.flip, styles.down)}>
+            {/* 位于前面的牌 */}
+            <div
+              className={classNames(styles.digital, styles.front, styles.num0)}
+            />
+            {/* 位于后面的牌 */}
+            <div
+              className={classNames(styles.digital, styles.back, styles.num1)}
+            />
+          </div>
+          {i % 2 === 1 && i !== n - 1 && (
+            <span className={styles.split}>:</span>
+          )}
+        </Fragment>
+      );
+    }
+    return panels;
+  };
+
   return (
-    <div>
+    <div className="flex-center" style={{ height: '100vh' }}>
       <button type="button" onClick={() => history.push('/')}>
         home
       </button>
-      <div className={classNames(styles.flip, styles.down)}>
-        {/* 位于前面的牌 */}
-        <div
-          className={classNames(styles.digital, styles.front, styles.num0)}
-        />
-        {/* 位于后面的牌 */}
-        <div className={classNames(styles.digital, styles.back, styles.num1)} />
-      </div>
+      <div className={styles.clock}>{renderPanel()}</div>
     </div>
   );
 }
