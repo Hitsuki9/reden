81b6c6ebebed01bab6c071b7bf6dc5703feee5c2
Hitsuki9 <31876031+Hitsuki9@users.noreply.github.com>
2021-04-09 17:19:39 +0800

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import WindiCSS from 'vite-plugin-windicss';
-import graphql from '@rollup/plugin-graphql';
 
 export default defineConfig({
-  plugins: [WindiCSS(), graphql(), reactRefresh()]
+  plugins: [WindiCSS(), reactRefresh()]
 });
